dea229789ee5a1da1bbad1e5196b48a997717132
DioDogHater <epiccoder2008@gmail.com>
2025-05-10 22:26:24 -0400

    Added advanced string example

--- advanced_string.h
+++ advanced_string.h
@@ -19,30 +19,37 @@ size_t _STRING_APPEND = 20;
 
 // Advanced string structure
 struct String{
-	char* arr;
+	char* str;
 	size_t size;
 };
 
 // Create a new string
 // Here, the string starts with size=1
 // That is because the appending will fail to allocate the extra byte for '\0' otherwise
-#define create_string(c) (struct String){NULL,0};
+#define create_string() (struct String){NULL,0};
 
 // Free / clear a string
-#define free_string(s) ({if((s).size) free((s).arr); (s).arr = NULL; (s).size = 0;})
+#define free_string(s) ({if((s).size) free((s).str); (s).str = NULL; (s).size = 0;})
 
 // Set the string to another string that may be formatted
 #define set_string(d,s,...) ({\
 	free_string((d));\
-	(d).arr = malloc(sizeof(char)*((d).size = strlen((s))+_STRING_APPEND+1));\
-	(d).size = sprintf((d).arr,(s),##__VA_ARGS__);\
+	(d).str = malloc(sizeof(char)*((d).size = strlen((s))+_STRING_APPEND+1));\
+	(d).size = sprintf((d).str,(s),##__VA_ARGS__);\
 })
 
 // Append a string to advanced string, may have a format
 #define append_string(d,s,...) ({\
-	(d).arr = realloc((d).arr,sizeof(char)*((d).size+strlen((s))+_STRING_APPEND+1));\
-	(d).size += sprintf((d).arr+(d).size,(s),##__VA_ARGS__);\
-	(d).arr[(d).size]='\0';\
+	(d).str = realloc((d).str,sizeof(char)*((d).size+strlen((s))+_STRING_APPEND+1));\
+	(d).size += sprintf((d).str+(d).size,(s),##__VA_ARGS__);\
+	(d).str[(d).size]='\0';\
+})
+
+// Allocate (n) bytes for string
+#define alloc_string(d,n) ({\
+	(d).size += (n)+1;\
+	(d).str = realloc((d).str,sizeof(char)*(d).size);\
+	(d).str[(d).size]='\0';\
 })
 
 #endif
---
+++ examples/advanced_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../advanced_string.h"
+
+int main(void){
+	// Set the maxmimum amount of bytes to allocate
+	// everytime we append text to the string
+	_STRING_APPEND = 256;
+
+	// Create an empty string
+	struct String str = create_string();
+	
+	printf("Enter \"quit\" to exit.\n");
+	
+	// Loop until user types quit
+	while(1){
+		// Buffer to hold input
+		char buffer[256];
+		fgets(buffer,255,stdin);
+
+		// Check if it's "quit"
+		if(!strcmp(buffer,"quit\n")) break;
+
+		// Append the input to the string
+		append_string(str,"%s",buffer);
+	}
+	
+	// Print the string
+	printf("\n\n====| This is what you just typed: |====\n%s\n",str.str);
+	
+	// Allocate the memory
+	free_string(str);
+
+	return 0;
+}
